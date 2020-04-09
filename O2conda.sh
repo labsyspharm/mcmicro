90f6d39563d794bbdbf343c8af2ac0ca550757a5
Artem Sokolov <artem.sokolov@gmail.com>
2020-04-09 18:00:53 -0400

    De-echoing

--- O2conda.sh
+++ O2conda.sh
@@ -2,7 +2,7 @@
 
 O2BASE="/n/groups/lsp/mcmicro"
 
-echo conda env create -f conda/ashlar.yml -p $O2BASE/conda/ashlar
-echo conda env create -f conda/unmicst.yml -p $O2BASE/conda/unmicst
-echo conda env create -f conda/s3seg.yml -p $O2BASE/conda/s3seg
-echo conda env create -f conda/quantification.yml -p $O2BASE/conda/quantification
+conda env create -f conda/ashlar.yml -p $O2BASE/conda/ashlar
+conda env create -f conda/unmicst.yml -p $O2BASE/conda/unmicst
+conda env create -f conda/s3seg.yml -p $O2BASE/conda/s3seg
+conda env create -f conda/quantification.yml -p $O2BASE/conda/quantification
