eba8e4862847302406f20d9d83fdfe790726257e
Artem Sokolov <artem.sokolov@gmail.com>
2020-04-22 16:40:42 -0400

    Added mc_ilastik conda environment

--- O2conda.sh
+++ O2conda.sh
@@ -6,3 +6,4 @@ conda env create -f conda/ashlar.yml -p $O2BASE/conda/ashlar
 conda env create -f conda/unmicst.yml -p $O2BASE/conda/unmicst
 conda env create -f conda/s3seg.yml -p $O2BASE/conda/s3seg
 conda env create -f conda/quantification.yml -p $O2BASE/conda/quantification
+conda env create -f conda/mc_ilastik.yml -p $O2BASE/conda/mc_ilastik
