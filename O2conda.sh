82fedb52cc25926a55a85b2fd8c2a5a975a2ce27
Artem Sokolov <artem.sokolov@gmail.com>
2020-06-06 12:27:32 -0400

    Unmicst update (#144)
    * UnMicst version bump
    
    * Pinned scikit-image version

--- O2conda.sh
+++ O2conda.sh
@@ -3,7 +3,7 @@
 # Usage: ./O2conda.sh /path/to/conda/destination
 
 conda env create -f conda/ashlar.yml         -p "$1/ashlar"
-conda env create -f conda/unmicst.yml        -p "$1/unmicst"
+conda env create -f conda/unmicst.yml        -p "$1/unmicst-2020-06-06"
 conda env create -f conda/s3seg.yml          -p "$1/s3seg"
 conda env create -f conda/quantification.yml -p "$1/quantification"
 conda env create -f conda/mc_ilastik.yml     -p "$1/mc_ilastik"
