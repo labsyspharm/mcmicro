b4917338bafbf4d15acab7edb40bcfff12479634
Artem Sokolov <artem.sokolov@gmail.com>
2020-06-12 11:56:47 -0400

    Prepared O2 environment for running naivestates

--- O2conda.sh
+++ O2conda.sh
@@ -7,3 +7,9 @@ conda env create -f conda/unmicst.yml        -p "$1/unmicst-2020-06-06"
 conda env create -f conda/s3seg.yml          -p "$1/s3seg"
 conda env create -f conda/quantification.yml -p "$1/quantification"
 conda env create -f conda/mc_ilastik.yml     -p "$1/mc_ilastik"
+conda env create -f conda/nstates.yml        -p "$1/naivestates"
+
+## Additional dependencies
+conda activate "$1/naivestates"
+R -s -e "devtools::install_github('labsyspharm/naivestates')"
+conda deactivate
