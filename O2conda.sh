a61efaf85a899b18f82f84b3296b1ad22aa9ce29
Artem Sokolov <artem.sokolov@gmail.com>
2020-07-27 13:58:57 -0400

    Environment variables for building pytiff

--- O2conda.sh
+++ O2conda.sh
@@ -2,6 +2,10 @@
 
 # Usage: ./O2conda.sh /path/to/conda/destination
 
+module load tiff conda2
+export LDFLAGS=`pkg-config --libs libtiff-4`
+export CFLAGS=`pkg-config --cflags libtiff-4`
+
 conda env create -f conda/ashlar.yml         -p "$1/ashlar"
 conda env create -f conda/coreo.yml          -p "$1/unetcoreo"
 conda env create -f conda/unmicst.yml        -p "$1/unmicst-2020-06-06"
