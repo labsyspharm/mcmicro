e758a1c9ca3df49267b55ae855ff937e70f85383
Artem Sokolov <artem.sokolov@gmail.com>
2020-09-02 23:34:27 -0400

    Separated out step-by-step exemplar-001 test

---
+++ tests/exemplar-001-ss.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r ~/data/downloads/exemplar-001-ss ~/data/
+./nextflow main.nf --in ~/data/exemplar-001-ss --start-at registration --stop-at registration
+./nextflow main.nf --in ~/data/exemplar-001-ss --start-at probability-maps --stop-at probability-maps --probability-maps ilastik
+./nextflow main.nf --in ~/data/exemplar-001-ss --start-at segmentation --stop-at segmentation --probability-maps ilastik
+./nextflow main.nf --in ~/data/exemplar-001-ss --start-at probability-maps --stop-at probability-maps --probability-maps unmicst
+./nextflow main.nf --in ~/data/exemplar-001-ss --start-at segmentation --stop-at segmentation --probability-maps unmicst
--- tests/exemplar-001.sh
+++ tests/exemplar-001.sh
@@ -2,11 +2,3 @@
 
 cp -r ~/data/downloads/exemplar-001 ~/data/
 ./nextflow main.nf --in ~/data/exemplar-001 --probability-maps all
-rm -r ~/data/exemplar-001
-
-cp -r ~/data/downloads/exemplar-001 ~/data/
-./nextflow main.nf --in ~/data/exemplar-001 --start-at registration --stop-at registration
-./nextflow main.nf --in ~/data/exemplar-001 --start-at probability-maps --stop-at probability-maps --probability-maps ilastik
-./nextflow main.nf --in ~/data/exemplar-001 --start-at segmentation --stop-at segmentation --probability-maps ilastik
-./nextflow main.nf --in ~/data/exemplar-001 --start-at probability-maps --stop-at probability-maps --probability-maps unmicst
-./nextflow main.nf --in ~/data/exemplar-001 --start-at segmentation --stop-at segmentation --probability-maps unmicst
