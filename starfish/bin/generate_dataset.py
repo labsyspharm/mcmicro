02f72d761a400d546955626c0f25b5a831aa04f1
Gleb Rukhovich <rukhovich@gmail.com>
2022-10-21 10:51:44 +0200

    Updated dataset generation

--- starfish/bin/generate_dataset.py
+++ starfish/bin/generate_dataset.py
@@ -1,21 +1,27 @@
 import os
 from starfish import data, FieldOfView
 import tifffile as tiff
-from starfish import Experiment
 from starfish.types import Axes
-import napari
+from pathlib import Path
+from typing import Union
+
 
 # Create sample raw dataset for tiling:
 exp = data.ISS(use_test_data=False)
 fov = exp['fov_000']
 primary = fov.get_image(FieldOfView.PRIMARY_IMAGES)
 
-save_dir = '/Users/segonzal/Documents/Repositories/imbast/data/primary'
-# Loop through data and save it:
-# Save nuclei (will focus for now only on primary images
-#tiff.imwrite(os.path.join(save_dir, '') ,fov.get_image('nuclei')
+# save_dir = '/Users/segonzal/Documents/Repositories/imbast/data/primary'
+save_dir = Path(os.getcwd()) / "sample_dataset"
 
-for ch in range(4):
-    for r in range(4):
-        img = primary.sel({Axes.ROUND: r, Axes.CH: ch}).xarray.squeeze()
-        tiff.imwrite(os.path.join(save_dir, 'r' + str(r) + '_' + 'CH' + str(ch) + '.tiff'), img)
+
+def generate_dataset(save_dir: Union[str, Path]):
+    save_dir = Path(save_dir)
+    for ch in range(4):
+        for r in range(4):
+            img = primary.sel({Axes.ROUND: r, Axes.CH: ch}).xarray.squeeze()
+            tiff.imwrite(save_dir / f"r{r}_CH{ch}.tiff", img)
+
+
+if __name__ == '__main__':
+    generate_dataset(save_dir)
